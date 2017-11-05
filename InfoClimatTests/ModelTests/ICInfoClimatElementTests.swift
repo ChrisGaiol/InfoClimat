fe6574c56b36f75fd664df95f32aff3cb8ce38a4
Christian Gaiola <Christian@MacBook-Air-de-Christian.local>
2017-11-05 22:58:35 +0100

    [ Tests ] - Add missing Unit Tests

--- InfoClimatTests/ModelTests/ICInfoClimatElementTests.swift
+++ InfoClimatTests/ModelTests/ICInfoClimatElementTests.swift
@@ -24,7 +24,30 @@ class ICInfoClimatElementTests: XCTestCase {
         let date = Date()
         let infoClimatElement = ICInfoClimatElement(forDate:date)
         
-        XCTAssertTrue(infoClimatElement.date == date)
+        XCTAssertEqual(infoClimatElement.date, date)
+        
+    }
+    
+    func testInitICInfoClimatElementFull() {
+        let date = Date()
+        let infoClimatElementSunnyDay = ICInfoClimatElement(forDate: date, temperature: 293.15, pluie: 0, humidite: 10, vent: 10, nebulosite: 10, risqueNeige: false)
+        
+        let infoClimatElementCloudyDay = ICInfoClimatElement(forDate: date, temperature: 280, pluie: 0, humidite: 10, vent: 10, nebulosite: 55, risqueNeige: false)
+        
+        let infoClimatElementRainyDay = ICInfoClimatElement(forDate: date, temperature: 280, pluie: 0.5, humidite: 10, vent: 10, nebulosite: 60, risqueNeige: false)
+        
+        XCTAssertEqual(infoClimatElementSunnyDay.date, date)
+        XCTAssertEqual(infoClimatElementSunnyDay.temperature, 293.15)
+        XCTAssertEqual(infoClimatElementSunnyDay.pluie, 0)
+        XCTAssertEqual(infoClimatElementSunnyDay.humidite, 10)
+        XCTAssertEqual(infoClimatElementSunnyDay.vent, 10)
+        XCTAssertEqual(infoClimatElementSunnyDay.nebulosite, 10)
+        XCTAssertEqual(infoClimatElementSunnyDay.risqueNeige, false)
+        XCTAssertEqual(infoClimatElementSunnyDay.temperatureCelsius, 20)
+        
+        XCTAssertEqual(infoClimatElementSunnyDay.climatType, .sunny)
+        XCTAssertEqual(infoClimatElementCloudyDay.climatType, .cloudy)
+        XCTAssertEqual(infoClimatElementRainyDay.climatType, .rainy)
         
     }
     
